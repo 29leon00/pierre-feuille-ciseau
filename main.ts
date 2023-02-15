7bcec2c287b242f46b40d7a0252711cd5ff14286
29leon00 <125478675+29leon00@users.noreply.github.com>
2023-02-15 15:29:34 +0000

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,34 @@
+let Hand = 0
+input.onButtonPressed(Button.A, function () {
+    Hand = randint(0, 2)
+    if (Hand == 0) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 500, 499, 255, 0, 750, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    } else if (Hand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    }
+})
+basic.forever(function () {
+	
+})
